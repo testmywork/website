2ec57c913a2c7b105b123c14011baa0d480722b6
Amrish Kushwaha <askmaurya48@gmail.com>
2019-05-13 23:02:59 +0530

    Initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { TestComponent } from './components/test/test.component';
+import { WorkspaceComponent } from './components/workspace/workspace.component'
+import { NotfoundComponent } from './components/notfound/notfound.component'
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'test',
+    component: TestComponent
+  },
+  {
+    path: 'workspace',
+    component: WorkspaceComponent
+  },
+  {
+    path: '**',
+    component: NotfoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { TestComponent } from './components/test/test.component';
+import { WorkspaceComponent } from './components/workspace/workspace.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TestComponent,
+    WorkspaceComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
